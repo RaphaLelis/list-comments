4cb6a63def3652de7cd05d6cd2f292f0d2afb3c0
Raphael Lelis <raphalelis@outlookc.com>
2022-05-04 15:51:55 -0300

    new component

--- src/components/Comments.vue
+++ src/components/Comments.vue
@@ -2,16 +2,7 @@
 	<div class="container" style="background-color: white;">
 		<h1>Comentários</h1>
 		<hr/>
-		<div class="form-all form-group">
-			<p>
-				<input placeholder="Nome" type="text" name="author" class="form-control" v-model="name" />
-			</p>
-			<p>
-				<textarea placeholder="Comentário" name="message" class="form-control" v-model="message"></textarea>
-			</p>
-			<button v-on:click="addComment" type="submit" class="btn btn-primary">Enviar</button>
-		</div>
-		<hr/>
+		<FormToDo v-on:add-todo = "addComments"></FormToDo>
 		<div class="list-group">
 			<div class="list-group-item" v-for="(comment, index) in allComments" :key="comment.id">
 				<span class="comment_author">Autor: <strong>{{comment.name}}</strong></span>
@@ -25,25 +16,20 @@
 </template>
 
 <script>
+import FormToDo from './FormToDo.vue'
+
 export default {
+	components:{
+		FormToDo
+	},
 	data() {
 		return {
-			comments: [],
-			name: '',
-			message: ''
+			comments: []
 		}
 	},
 	methods: {
-		addComment() {
-			if (this.message.trim() === '') {
-				return;
-			}
-			this.comments.push({
-				name: this.name,
-				message: this.message
-			});
-			this.name = '',
-				this.message = ''
+		addComments(comment){
+			this.comments.push(comment);
 		},
 		deleteComment(index) {
 			this.comments.splice(index, 1);
